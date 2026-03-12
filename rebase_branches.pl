b6b9e2286940024bada7e3e5f8939fa5703b1b8c
Kyle M Hall <kyle@bywatersolutions.com>
2026-03-12 17:50:13 -0400

    Rittenhouse - Update files

--- rebase_branches.pl
+++ rebase_branches.pl
@@ -25,7 +25,7 @@ my ( $org, $repo ) = split m{/}, $full_repo, 2;
 say "Org:  $org";
 say "Repo: $repo";
 
-say "\nNot koha-plugin-edifact-enhanced, exiting" && exit 0 unless $repo eq "koha-plugin-edifact-enhanced";
+say "\nNot koha-plugin-edifact-rittenhouse, exiting" && exit 0 unless $repo eq "koha-plugin-edifact-rittenhouse";
 
 qx(git remote);
 qx(git fetch origin);
@@ -39,8 +39,8 @@ say "FOUND REPOS " . Data::Dumper::Dumper( \@repos );
 
 my $failures = 0;
 foreach my $repo (@repos) {
-    next if $repo eq 'koha-plugin-edifact-enhanced';
-    next if $repo eq 'koha-plugin-edifact-enhanced-docs';
+    next if $repo eq 'koha-plugin-edifact-rittenhouse';
+    next if $repo eq 'koha-plugin-edifact-rittenhouse-docs';
 
     say "WORKING ON $repo";
 
